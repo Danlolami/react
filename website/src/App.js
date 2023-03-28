ae44ea9fe9a11815a61972f9e40edcccb1bae506
Danlolami <108428020+Danlolami@users.noreply.github.com>
2023-03-28 22:45:52 +0100

    Add files via upload

---
+++ website/src/App.js
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import './App.css';
+import Header from './Header';
+import Main from './Main';
+
+function App() {
+  return (
+    <>
+    <Header></Header>
+    <Main></Main>
+    </>
+  );
+}
+
+export default App;
